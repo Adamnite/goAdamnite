ea13f0b983e4d2d9b2e89a1c2c5def83c462dfe3
Arch222 <66220753+Arch222@users.noreply.github.com>
2022-02-18 16:58:04 -0500

    Update Block Structure and Transaction Structure
    It is now specific to Adamnite's Design Documentation. Also added some ToDo things.

--- core/types/block.go
+++ core/types/block.go
@@ -1,26 +1,38 @@
-package types
+//Package Main contains the data structures that make up Adamnite's core protocol
+package main
 
 import (
 	"math/big"
-
+  "sync/atomic"
+	"time"
 	"github.com/adamnite/go-adamnite/common"
 )
 
-type Blocks []*Block
 
 type BlockHeader struct {
-	ParentHash common.Hash    `json:"parentHash" gencodec:"required"`
-	UncleHash  common.Hash    `json:"uncleHash" gencodec:"required"`
-	Witness    common.Address `json:"validator" gencodec:"required"`
-	TxHash     common.Hash    `json:"txHash" gencodec:"required"`
+	PreviousHash common.Hash  `json:"PreviousHash" gencodec:"required"`
+  Hash       common.Hash    `json:"Hash" gencodec:"required"`
+  Time       uint64         `json:"timestamp" gencodec:"required"`
+	Witness    common.Address `json:"witness" gencodec:"required"`
+  WitnessListHash     common.Hash       `json:"witnessListHash" gencodec:"required"`
+  NetFee     uint64         `json:"NetFee" gencodec:"required"`
+  NetStorage uint64         `json:"NetStorage" gencodec:"required"`
 	Number     *big.Int       `json:"number" gencodec:"required"`
-	GasLimit   uint64         `json:"gasLimit" gencodec:"required"`
-	GasUsed    uint64         `json:"gasUsed" gencodec:"required"`
-	Time       uint64         `json:"timestamp" gencodec:"required"`
+  Signature  common.Hash    `json:"number" gencodec:"required"`
+  Size       uint64         `json:"Size" gencodec:"required"`
+	TransactionRoot   common.Hash        `json:"TransactionRoot" gencodec:"required"`
 }
 
 type Block struct {
 	header       *BlockHeader
-	uncles       []*BlockHeader
-	transactions Transactions
+	transactionList Transactions
+  witnessList []*BlockHeader
+  //cache values, similar to GoETH
+  hash atomic.Value
+	size atomic.Value
+
 }
+
+//// TODO: Implement structure for creating a new block, create a new block with
+//header data, proper encoding of data, basic header checks (for example, check if the block number is too high),
+// decoding of data, and functions to retreive various header data and hashes.
--- core/types/transaction.go
+++ core/types/transaction.go
@@ -1,37 +1,64 @@
+
 package types
 
 import (
 	"math/big"
 	"time"
-
+  "sync/atomic"
 	"github.com/adamnite/go-adamnite/common"
 )
 
-// Transactions implements DerivableList for transactions.
-type Transactions []*Transaction
+var(
+  ErrorCryptoSignature    = errors.New("Invalid Elliptic Curve Values")
+  ErrorEmptyTX            = errors.New("Empty Transaction Data")
+  ErrorLowFee             = errors.New("Fee lower than minimum fee dicated by network")
+  ErrorInvalidTransaction = errors.New("Transaction Balance is too large")
+
+)
+
+
 
 // Transaction is an Adamnite transaction.
 type Transaction struct {
-	txdata TxData
-	time   time.Time
+	InnerData Transaction_Data
+	timestamp time.Time
+  //Cache Values, similar to GoETH
+  From atomic.Value
+  hash atomic.Value
+  size atomic.Value
 }
+
+
+func CreateTx(InnerData Transaction_Data) *Transaction{
+  transaction := new(Transaction)
+  transaction.Decode(InnerData.copy(),0) //We will need to some functions for decoding and encoding data types
+                                        //This can be RLP or our own implementation.
+  return transaction
+}
+
 
 // TxData is the underlying data of a transaction.
-type TxData interface {
-	// txType returns the type ID
-	txType() byte
-
+type Transaction_Data interface {
 	// copy creates a deep copy and initializes all fields
-	copy() TxData
-
-	chainID() *big.Int
-	data() []byte
-	gas() uint64
-	gasPrice() *big.Int
-	value() *big.Int
+	copy() Transaction_Data
+	chain_TYPE() *big.Int
+  to() *common.Public
+  amount() *big.Int
+	message() []byte
+  message_size() *big.Int //Size of message in bytes
+	ATE_MAX() uint64 //Transaction Fee Limit that the user encoded
+	ATE_price() *big.Int //Actual Transaction Fee
 	nonce() uint64
-	to() *common.Address
 
-	rawSignatureValues() (v, r, s *big.Int)
-	setSignatureValues(chainID, v, r, s *big.Int)
+	rawSignature() (v, r, s *big.Int)
+	setSignatue(chain_TYPE, v, r, s *big.Int)
 }
+
+
+
+type Transactions []*Transaction
+
+
+//// TODO: Implement structure for creating a new transaction, basic message transactions (will do this weekend),
+//proper encoding and decoding of transactions, fetch operations (the fetching of various transaction releated data),
+//and some stack operations such as shift. 
