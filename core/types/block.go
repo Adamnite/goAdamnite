35a797fd5370c80ed8305d3a4fdf795925f883b7
Arch222 <66220753+Arch222@users.noreply.github.com>
2022-02-18 19:59:27 -0500

    Update block.go

--- core/types/block.go
+++ core/types/block.go
@@ -10,26 +10,26 @@ import (
 
 
 type BlockHeader struct {
-	PreviousHash common.Hash  `json:"PreviousHash" gencodec:"required"`
+  PreviousHash common.Hash  `json:"PreviousHash" gencodec:"required"`
   Hash       common.Hash    `json:"Hash" gencodec:"required"`
   Time       uint64         `json:"timestamp" gencodec:"required"`
-	Witness    common.Address `json:"witness" gencodec:"required"`
+  Witness    common.Address `json:"witness" gencodec:"required"`
   WitnessListHash     common.Hash       `json:"witnessListHash" gencodec:"required"`
   NetFee     uint64         `json:"NetFee" gencodec:"required"`
   NetStorage uint64         `json:"NetStorage" gencodec:"required"`
-	Number     *big.Int       `json:"number" gencodec:"required"`
+  Number     *big.Int       `json:"number" gencodec:"required"`
   Signature  common.Hash    `json:"number" gencodec:"required"`
   Size       uint64         `json:"Size" gencodec:"required"`
-	TransactionRoot   common.Hash        `json:"TransactionRoot" gencodec:"required"`
+  TransactionRoot   common.Hash        `json:"TransactionRoot" gencodec:"required"`
 }
 
 type Block struct {
-	header       *BlockHeader
-	transactionList Transactions
+  header       *BlockHeader
+  transactionList Transactions
   witnessList []*BlockHeader
   //cache values, similar to GoETH
   hash atomic.Value
-	size atomic.Value
+  size atomic.Value
 
 }
 
