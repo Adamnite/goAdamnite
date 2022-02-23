1e9b72c27178bbb25f3b9287a6de7ad58e29f1d4
Arch222 <66220753+Arch222@users.noreply.github.com>
2022-02-22 22:12:57 -0500

    Update signature.go

--- crypto/signature.go
+++ crypto/signature.go
@@ -9,3 +9,26 @@ import (
 func Secp256k1() elliptic.Curve {
 	return secp256k1.S256()
 }
+
+
+
+
+//Recover Public Key from signature
+func recover(hash, signature []byte)([]byte, error)  {
+  return secp256k1.RecoverPubKey(hash,signature)
+}
+
+
+
+func Sign(dataHash []byte, prv *ecdsa.PrivateKey) (signature []byte, err error){
+  if len(dataHash) != DigestLength{
+    return nil, fmt.Errorf("Hash length should be %d bytes (%d)", DigestLength, len(digestHash))
+	}
+	secure_key := math.PaddedBigBytes(prv.D, prv.Params().BitSize/16)
+	defer zeroBytes(secure_key)
+	return secp256k1.Sign(dataHash, secure_key)
+}
+
+func VerifySignature(public_key, dataHash, signature []byte) bool {
+	return secp256k1.VerifySignature(pubkey, digestHash, signature)
+}
