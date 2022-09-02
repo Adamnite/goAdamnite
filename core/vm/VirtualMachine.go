4e119ac6d364d88614559d861a748d02023bf67b
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2022-09-01 20:03:48 -0400

    parses opcode strings to opcodes.

--- core/vm/VirtualMachine.go
+++ core/vm/VirtualMachine.go
@@ -1,5 +1,10 @@
 package vm
 
+import (
+	"strconv"
+	"strings"
+)
+
 type VirtualMachine interface {
 	//functions that will be fully implemented later
 	step()
@@ -24,7 +29,7 @@ type Storage struct {
 type operation struct {
 	//generate based on passed opcode list
 	opcode int8
-	params []int64 //any params that may be passed with the opcode?
+	params []int8 //any params that may be passed with the opcode?
 }
 
 func (m Machine) step() {
@@ -62,8 +67,30 @@ func newVirtualMachine(code []string, storage Storage) *Machine {
 }
 func parseCodeToOpcodes(code []string) []operation {
 	// convert all the wasm data to opcodes with their function
-	var op []operation
-	return op
+	var operations []operation
+	for _, s := range code {
+		values := strings.Split(s, " ")
+
+		opcode, err := strconv.ParseInt(values[0], 16, 8)
+		var params []int8
+		if err != nil {
+			//TODO: check if err is used
+		}
+
+		for i, a := range values {
+			if i != 0 {
+				param, err := strconv.ParseInt(a, 16, 8)
+				if err != nil {
+					//TODO: check if err is used
+				}
+				params = append(params, int8(param))
+			}
+		}
+		operations = append(operations, operation{
+			opcode: int8(opcode),
+			params: params})
+	}
+	return operations
 }
 
 func popFromOpcodeStack(ops []operation) (operation, []operation) {
--- core/vm/VirtualMachine_test.go
+++ core/vm/VirtualMachine_test.go
@@ -5,12 +5,48 @@ import (
 )
 
 func Test_newVirtualMachine(t *testing.T) {
-	var codes []string
-	vm := newVirtualMachine(codes, Storage{})
+	vm := newVirtualMachine(generateTestWasm(), Storage{})
+	// TODO: make this fail. Once the wasm is actually ran, it will fail!
 	println(vm)
 	vm.step()
 	println(len(vm.vmMemory) == 0)
 	vm.run()
 	println(len(vm.vmMemory) == 0)
 	// if we make it this far, ill count it as a success
+}
+func Test_parseCodeToOpcodes(t *testing.T) {
+	answer := []int8{0, 97, 115, 109, 1, 0, 0, 0, 1, 0, 1, 96, 1, 115, 1, 115, 6, 3, 0, 1, 0, 2, 10, 0, 1}
+	result := parseCodeToOpcodes(generateTestWasm())
+	if result[0].opcode != int8(answer[0]) {
+		t.Fail()
+	}
+	for i, v := range result[0].params {
+		if v != int8(answer[i+1]) {
+			t.Fail()
+		}
+
+	}
+}
+
+func generateTestWasm() []string {
+	// https://en.wikipedia.org/wiki/WebAssembly code grabbed from
+	return []string{
+		"00 61 73 6D 01 00 00 00",
+		"01 00 01 60 01 73 01 73 06",
+		"03 00 01 00 02",
+		"0A 00 01",
+		"00 00",
+		"20 00",
+		"50",
+		"04 7E",
+		"42 01",
+		"05",
+		"20 00",
+		"20 00",
+		"42 01",
+		"7D",
+		"10 00",
+		"7E",
+		"0B",
+		"0B 15 17"}
 }
