f6652ef126ef80357f83a47e685f65ff87be8a6d
Arch222 <66220753+Arch222@users.noreply.github.com>
2022-11-30 21:05:50 -0500

    Update bootnodes.go

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -2,4 +2,5 @@ package params
 
 var MainnetBootnodes = []string{
 	"gnite://04be77bef6deb3d8886efe6793321a79c9d03b22616c5a8557275ac46ccb757aed60af739fa80a442311bc56de497cc69c65dadea5fe5a7ab78c48aa90a209c3c0@127.0.0.1:0?udp=3880",
+	"gnite://0413fbf3921cf0b9b005fbc3981b14dfa20edbcb9ee0d7bf0683b3eb587636272b9d90c66a3e2ad1dd695c4758551fa230abf17ae97c48b93bdce05c047f5c512d@127.0.0.1:0?udp=3880", // bootnode-aws-adamnite-seed-01
 }
