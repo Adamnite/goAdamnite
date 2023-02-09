2f38e81961a55d400186d99769e4309395f6deee
Arch222 <66220753+Arch222@users.noreply.github.com>
2023-02-09 13:26:00 -0500

    Delete params directory

--- params/bootnodes.go
+++
@@ -1,5 +0,0 @@
-package params
-
-var MainnetBootnodes = []string{
-	"enode://90be1f07759110172e6dd62722b2468b214a9e24814e6822b5904754bdbac22f57eb234b56581502eea88f8ff9d018fe1487be20cfa10fce1981a82a09af6386@192.168.112.109:3880", // bootnode-aws-ap-southeast-1-001
-}
