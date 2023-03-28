23ce2adc781760886327ed9b61d06fe1d413d624
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2023-03-27 20:10:47 -0400

    nite denominations were not importable

--- params/nite_denominations.go
+++ params/nite_denominations.go
@@ -1,14 +1,13 @@
 //Copyright 2022 TheGoAdamnite Authors
 
-
 package params
 
-//Adamnite Denominators (only three are used, we are getting rid of the cryptographer references to avoid possible infrigement (not likely) 
+//Adamnite Denominators (only three are used, we are getting rid of the cryptographer references to avoid possible infrigement (not likely)
 //and because dubnite sounds a lot cooler for the base unit.
 
 const (
-	dubnite = 1 //Analagous to Wei in Ethereum
-	decnite = 1e10 //Analagous to GWei in Ethereum
-	nite = 1e20 //Analgous to Eth in Ethereum
+	Dubnite = 1    //Analagous to Wei in Ethereum
+	Decnite = 1e10 //Analagous to GWei in Ethereum
+	Nite    = 1e20 //Analgous to Eth in Ethereum
 
 )
