c1900df40f2da240cd05ba996390bf80a6781d66
Marin Peko <marinpeko5@gmail.com>
2023-04-29 22:47:42 +0200

    Delete server test temporarily

--- rpc/common.go
+++ rpc/common.go
@@ -1,24 +1,8 @@
 package rpc
 
 import (
-	"math/big"
-
 	"github.com/vmihailenco/msgpack/v5"
 )
-
-type BigIntRPC struct {
-	Value []byte
-}
-
-func (b *BigIntRPC) toBigInt() *big.Int {
-	return big.NewInt(0).SetBytes(b.Value)
-}
-func BigIntReplyFromBytes(val []byte) BigIntRPC {
-	return BigIntRPC{Value: val}
-}
-func BigIntReplyFromBigInt(val big.Int) BigIntRPC {
-	return BigIntRPC{Value: val.Bytes()}
-}
 
 func Encode(v interface{}) ([]byte, error) {
 	return msgpack.Marshal(v)
