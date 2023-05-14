9f0691bffa99261c54d01513e4a9049dbf8ac7a0
Marin Peko <marinpeko5@gmail.com>
2023-05-14 20:38:27 +0200

    Add signing functions to account

--- utils/accounts/account.go
+++ utils/accounts/account.go
@@ -3,6 +3,7 @@ package accounts
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"crypto/sha256"
 	"crypto/sha512"
 	"crypto/rand"
 	"log"
@@ -32,6 +33,19 @@ func GenerateAccount() (*Account, error) {
 		PublicKey : publicKey,
 		PrivateKey: privateKey,
 	}, nil
+}
+
+func (a *Account) Sign(data []byte) ([]byte, error) {
+	signature, err := secp256k1.Sign(sha256Hash(data), a.PrivateKey)
+	if err != nil {
+		log.Printf("Signing error: %s", err)
+		return nil, err
+	}
+	return signature, nil
+}
+
+func (a *Account) Verify(data []byte, signature []byte) bool {
+	return secp256k1.VerifySignature(a.PublicKey, sha256Hash(data), signature)
 }
 
 func generateKeys() (rawPublicKey, rawPrivateKey []byte, err error) {
@@ -79,6 +93,12 @@ func b58encode(data []byte) []byte {
 
 func ripemd160Hash(data []byte) []byte {
 	hasher := ripemd160.New()
+	hasher.Write(data)
+	return hasher.Sum(nil)
+}
+
+func sha256Hash(data []byte) []byte {
+	hasher := sha256.New()
 	hasher.Write(data)
 	return hasher.Sum(nil)
 }
--- utils/accounts/account_test.go
+++ utils/accounts/account_test.go
@@ -16,3 +16,18 @@ func TestGenerateAccount(t *testing.T) {
 	assert.Equal(t, 65, len(account.PublicKey), "Public key should be 65 bytes long")
 	assert.Equal(t, 32, len(account.PrivateKey), "Private key should be 32 bytes long")
 }
+
+func TestSignData(t *testing.T) {
+	account, err := GenerateAccount()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data := []byte("Test message")
+	signature, err := account.Sign(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, 65, len(signature), "Signature should be 65 bytes long")
+	assert.True(t, account.Verify(data, signature), "Signature should be verified")
+}
