5930f0ef5c6d87ed416563f6013bfeb0ff0a6ea9
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2023-05-27 00:50:58 -0400

    base CLI for messaging

--- caesar/caesarNode.go
+++ caesar/caesarNode.go
@@ -1,6 +1,8 @@
 package caesar
 
 import (
+	"sort"
+
 	"github.com/adamnite/go-adamnite/common"
 	"github.com/adamnite/go-adamnite/networking"
 	"github.com/adamnite/go-adamnite/utils"
@@ -9,11 +11,12 @@ import (
 
 
 type CaesarNode struct {
-	netHandler     *networking.NetNode
-	signerSet      *accounts.Account
-	msgByHash      map[string]*utils.CaesarMessage
-	msgByRecipient map[common.Address][]*utils.CaesarMessage
-	msgBySender    map[common.Address][]*utils.CaesarMessage
+	netHandler        *networking.NetNode
+	signerSet         *accounts.Account
+	msgByHash         map[string]*utils.CaesarMessage
+	msgByRecipient    map[common.Address][]*utils.CaesarMessage
+	msgBySender       map[common.Address][]*utils.CaesarMessage
+	NewMessageUpdater func(*utils.CaesarMessage)
 }
 
 func NewCaesarNode(sendingKey *accounts.Account) *CaesarNode {
@@ -37,13 +40,61 @@ func (cn *CaesarNode) Startup() error {
 	cn.netHandler.AddMessagingCapabilities(
 		cn.AddMessage,
 	)
-
 	return nil
+}
+func (cn CaesarNode) GetConnectionPoint() string {
+	return cn.netHandler.GetOwnContact().ConnectionString
+}
+func (cn CaesarNode) GetMessagesBetween(a, b accounts.Account) []*utils.CaesarMessage {
+	ansMessages := []*utils.CaesarMessage{}
+	for _, msg := range cn.msgBySender[a.Address] {
+		if msg.To.Address == b.Address {
+			ansMessages = append(ansMessages, msg)
+		}
+	}
+	for _, msg := range cn.msgBySender[b.Address] {
+		if msg.To.Address == a.Address {
+			ansMessages = append(ansMessages, msg)
+		}
+	}
+	//sort them to be in order, cause why not
+	sort.Slice(ansMessages, func(i, j int) bool {
+		return ansMessages[i].InitialTime.Before(ansMessages[j].InitialTime)
+	}) //TODO: why not might be for important performance reasons...
+
+	return ansMessages
+}
+
+// connect this to a seed node that it can propagate from
+func (cn *CaesarNode) ConnectToNetworkFrom(seedConnectionPoint string) error {
+	//TODO: once we have a seed node running, we should add a " seedConnectionPoint == '', use our known default"
+	if err := cn.netHandler.ConnectToSeed(seedConnectionPoint); err != nil {
+		return err
+	}
+	return cn.FillNetworking(false)
+}
+
+// handle all the network stuff, and fill this nodes connections (pretty wide considering how little processor power this server type should take)
+func (cn *CaesarNode) FillNetworking(overrideSetLimits bool) error {
+	if overrideSetLimits {
+		cn.netHandler.SetMaxGreyList(0)
+		cn.netHandler.SetMaxConnections(64)
+	}
+	err := cn.netHandler.SprawlConnections(2, 0)
+	if err != nil && err != networking.ErrNoNewConnectionsMade {
+		//reaching every node on the network isn't the worst, and no further problems would occur filling the connections then
+		return err
+	}
+
+	return cn.netHandler.FillOpenConnections()
 }
 
 func (cn *CaesarNode) AddMessage(msg *utils.CaesarMessage) {
 	if _, exists := cn.msgByHash[string(msg.Hash())]; exists {
 		return
+	}
+	if cn.NewMessageUpdater != nil {
+		cn.NewMessageUpdater(msg)
 	}
 
 	cn.msgByHash[string(msg.Hash())] = msg
