441d495d3f5d2f802b9a4c58a38c5ba485579309
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2023-06-19 19:19:04 -0400

    threadSafeInts

---
+++ utils/safe/threadSafeInt.go
@@ -0,0 +1,36 @@
+package safe
+
+import "sync"
+
+//sometimes you need a number to be passed between threads.
+
+type SafeInt struct {
+	lock  sync.Mutex
+	value int
+}
+
+func NewSafeInt(value int) *SafeInt {
+	return &SafeInt{
+		value: value,
+	}
+}
+
+// returns the sum, as well as storing it locally.
+func (tsi *SafeInt) Add(x int) int {
+	tsi.lock.Lock()
+	defer tsi.lock.Unlock()
+	tsi.value += x
+	return tsi.value
+}
+
+func (tsi *SafeInt) Get() int {
+	tsi.lock.Lock()
+	defer tsi.lock.Unlock()
+	return tsi.value
+}
+
+func (tsi *SafeInt) Set(value int) {
+	tsi.lock.Lock()
+	defer tsi.lock.Unlock()
+	tsi.value = value
+}
