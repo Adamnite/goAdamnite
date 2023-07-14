4320cb3c2e37ef660ad1a613c831f6cf6b1c09cd
Marin Peko <marinpeko5@gmail.com>
2023-07-14 19:01:03 +0200

    Fix tests

--- VM/cli/cmd/upload_test.go
+++ VM/cli/cmd/upload_test.go
@@ -1,18 +1,21 @@
 package cmd
 
 import (
-	"encoding/hex"
+	"log"
 	"testing"
+	"encoding/hex"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTriggerUpload(t *testing.T) {
-	readBytes, _ := hex.DecodeString("0061736d0100000001070160027e7e017e03020100070a010661646454776f00000a09010700200020017c0b000a046e616d650203010000")
+func TestUpload(t *testing.T) {
+	rawBytes, err := hex.DecodeString("0061736d0100000001070160027e7e017e03020100070a010661646454776f00000a09010700200020017c0b000a046e616d650203010000")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	dbHost = "http://0.0.0.0:5000/"
 	gas = 1000
 
-	assert.True(t, triggerUpload(readBytes), "upload failed.")
-	print() //idk why this is needed, but it was failing without it...
+	assert.True(t, upload(rawBytes), "upload failed.")
 }
